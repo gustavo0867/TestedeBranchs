89915f5e7199ebd53db1ca864f733a1f6f8a3e98
[200~gustavo0867 <gbrasilreis08@gmail.com>
2024-03-15 12:41:32 -0400

    adicionando java

---
+++ teste/src/teste/Interacao.java
@@ -0,0 +1,16 @@
+package teste;
+
+public class Interacao {
+	
+	public int vida=10;
+	
+	public void seila() {
+		
+		
+	System.out.println("s");
+	}
+	
+	public void perder() {
+	vida--;	
+	}
+}
---
+++ teste/src/teste/game.java
@@ -0,0 +1,18 @@
+package teste;
+
+public class game {
+	
+	public static void main(String[] args ) {
+		int vida = 10;
+		String nome ="Gustavo";
+		
+		
+		System.out.println("O meu nome é:"+nome);
+		
+		for(int i=0;i<10;i++) {
+			
+			vida=vida+1;
+		}
+		System.out.println("A minha idade é:"+vida);
+	}
+}
